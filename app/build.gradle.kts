06277a3aeef3d9b8277d55a29f58abdc6d65372c
Nils Druyen <nils.druyen@fressnapf.com>
2023-08-19 14:01:19 +0200

    Switch to compose alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
 
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("dev.chrisbanes.compose:compose-bom:2023.09.00-alpha02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
